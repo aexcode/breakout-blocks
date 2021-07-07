fb5711a57787c4326a3d6fb81ccf4948a4957e5d
Ayla Ex <aylaex@protonmail.com>
2021-07-07 19:25:31 -0400

    add play and restart functionality

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
+let animationFrameId
+
+const startBtn = document.createElement('button')
+startBtn.textContent = 'Play Game'
+startBtn.classList.add('button')
+startBtn.style.display = 'none'
+document.getElementsByTagName('body')[0].appendChild(startBtn)
+
+const restartBtn = document.createElement('button')
+restartBtn.textContent = 'Restart'
+restartBtn.classList.add('button')
+restartBtn.style.display = 'none'
+document.getElementsByTagName('body')[0].appendChild(restartBtn)
 
 canvas.width = window.innerWidth < 900 ? window.innerWidth - 50 : 900
 canvas.height = window.innerHeight < 700 ? window.innerHeight - 50 : 700
@@ -7,46 +20,41 @@ canvas.height = window.innerHeight < 700 ? window.innerHeight - 50 : 700
 window.addEventListener('resize', () => {
   canvas.width = window.innerWidth < 900 ? window.innerWidth - 50 : 900
   canvas.height = window.innerHeight < 700 ? window.innerHeight - 50 : 700
+  cancelAnimationFrame(animationFrameID)
+  draw()
 })
 
+startBtn.addEventListener('click', handleStartGame, false)
+restartBtn.addEventListener('click', handleStartGame, false)
 document.addEventListener('keydown', handleKeyDown, false)
 document.addEventListener('keyup', handleKeyUp, false)
 document.addEventListener('mousemove', handleMouseMove, false)
 
-const ball = {
-  x: canvas.width / 2,
-  y: canvas.height - 30,
-  dx: 4,
-  dy: 4,
-  radius: canvas.height * 0.02,
+let ball
+let paddle
+let button
+const game = {
+  status: 'pre', // 'pre' || 'in-progress' ||'completed'
+  previous: false,
 }
 
-const paddle = {
-  x: 0,
-  y: canvas.height - 30,
-  dx: 7,
-  height: 30,
-  width: canvas.width / 4,
-}
-
-const buttons = {
-  right: false,
-  left: false,
+function handleStartGame() {
+  game.status = 'in-progress'
 }
 
 function handleKeyDown(e) {
   if (e.key == 'Right' || e.key == 'ArrowRight') {
-    buttons.right = true
+    button.right = true
   } else if (e.key == 'Left' || e.key == 'ArrowLeft') {
-    buttons.left = true
+    button.left = true
   }
 }
 
 function handleKeyUp(e) {
   if (e.key == 'Right' || e.key == 'ArrowRight') {
-    buttons.right = false
+    button.right = false
   } else if (e.key == 'Left' || e.key == 'ArrowLeft') {
-    buttons.left = false
+    button.left = false
   }
 }
 
@@ -81,13 +89,13 @@ function drawPaddle() {
 }
 
 function updatePaddle() {
-  if (buttons.right) {
+  if (button.right) {
     if (paddle.x >= canvas.width - paddle.width / 2) {
       paddle.x = canvas.width - paddle.width / 2
     } else {
       paddle.x += 7
     }
-  } else if (buttons.left) {
+  } else if (button.left) {
     if (paddle.x <= 0 - paddle.width / 2) {
       paddle.x = 0 - paddle.width / 2
     } else {
@@ -99,31 +107,93 @@ function updatePaddle() {
 function collisionDetection() {
   const left = 0 + ball.radius
   const right = canvas.width - ball.radius
-  // if ball is touching left or right, reverse horizontal direction
+  const top = 0 + ball.radius
+  const bottom = canvas.height - ball.radius
+  const ballLeft = ball.x - ball.radius
+  const ballRight = ball.x + ball.radius
+
   if (ball.x < left || ball.x > right) {
     ball.dx = -ball.dx
   }
 
-  const top = 0 + ball.radius
-  const bottom = canvas.height - ball.radius
-  // if ball is touching top or bottom, reverse vertical direction
-  if (ball.y < top || ball.y > bottom) {
+  if (ball.y < top) {
     ball.dy = -ball.dy
   }
+
+  if (
+    ballLeft > paddle.x &&
+    ballRight < paddle.x + paddle.width &&
+    ball.y - ball.radius > paddle.y - ball.radius
+  ) {
+    ball.dy = -ball.dy
+  }
+
+  if (ball.y > bottom) {
+    game.previous = true
+    game.status = 'pre'
+  }
+}
+
+function preGame() {
+  ball = {
+    x: canvas.width / 2,
+    y: canvas.height - 30,
+    dx: 4,
+    dy: 4,
+    radius: canvas.height * 0.02,
+  }
+
+  paddle = {
+    x: 0,
+    y: canvas.height - 30,
+    dx: 7,
+    height: 30,
+    width: canvas.width / 4,
+  }
+  button = {
+    right: false,
+    left: false,
+    width: 200,
+  }
+
+  if (game.previous) {
+    restartBtn.style.display = 'block'
+  } else {
+    startBtn.style.display = 'block'
+  }
+}
+
+function endGame() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  drawBackground()
 }
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
-
   drawBackground()
-  drawBall()
-  drawPaddle()
 
-  updateBall()
-  updatePaddle()
+  switch (game.status) {
+    case 'pre':
+      preGame()
+      break
+    case 'in-progress':
+      restartBtn.style.display = 'none'
+      startBtn.style.display = 'none'
 
-  collisionDetection()
-  requestAnimationFrame(draw)
+      drawBall()
+      drawPaddle()
+
+      updateBall()
+      updatePaddle()
+
+      collisionDetection()
+      break
+    case 'completed':
+      endGame()
+      break
+  }
+
+  animationFrameID = requestAnimationFrame(draw)
 }
 
 draw()
